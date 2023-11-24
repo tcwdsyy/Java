9e13aa935e6c16925add5aa352044d9604ac4ff5
shiyuyang <1175826687@qq.com>
2023-11-24 14:08:14 +0800

    Added Abstract.java and StaticFinal.java

---
+++ src/main/java/syy/learning/Abstract.java
@@ -0,0 +1,35 @@
+package syy.learning;
+
+public class Abstract {
+    // Animal a = new Animal();
+    public static void main(String[] args){
+        Animal a = new Dog();
+        a.eat();
+        a.sleep();
+    }
+}
+
+abstract class Animal{
+    String breed;
+    int age;
+    String sex;
+    public Animal(){}
+
+    public abstract void eat();
+
+    public void sleep(){
+        System.out.println("The animal is sleeping");
+    }
+}
+
+class Dog extends Animal{
+    public void eat(){
+        System.out.println("The dog is eating");
+    }
+
+
+
+    public void sleep(){
+        System.out.println("The dog is sleeping");
+    }
+}
---
+++ src/main/java/syy/learning/StaticFinal.java
@@ -0,0 +1,40 @@
+package syy.learning;
+
+public class StaticFinal {
+    public static void main(String[] args) {
+        MyClas m = new MyClas();
+        m.method();
+
+        System.out.println(MyClas.getSField());
+    }
+}
+
+class MyClas{
+    private static String sField;
+    static{// 类加载静态代码仅执行一次
+        System.out.println("生成静态属性");
+        sField="hello world";
+    }
+
+    public static String getSField(){
+        return sField;
+    }
+
+    public static void changeSField(){
+        System.out.println("called");
+        sField="called once";
+    }
+
+    public void method(){
+        MyClas.changeSField();//non-static method calls static method
+    }
+
+//    final修饰类：此类不能被继承
+//    final修饰方法：此方法不能被覆盖
+//    final修饰变量：此变量值不能被改变（无初始值，只允许赋值一次）
+//    局部变量：显示初始化
+//    实例变量：实现初始化、动态代码快、构造方法
+//    静态常量：显示初始化、静态代码块
+//    基本数据类型常量：值不可变
+//    引用数据类型常量：地址不可变
+}
